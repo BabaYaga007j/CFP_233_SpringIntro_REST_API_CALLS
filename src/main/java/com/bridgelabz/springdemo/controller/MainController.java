3d8281f22d955296b52e5ced58ef7ac9482ea7d1
BabaYaga007j <soumyasourav35@gmail.com>
2023-04-07 10:05:36 +0530

    REST call for displaying Hello message

--- src/main/java/com/bridgelabz/springdemo/controller/MainController.java
+++ src/main/java/com/bridgelabz/springdemo/controller/MainController.java
@@ -5,9 +5,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
+    //UC1
     @RequestMapping (value="/message",method = RequestMethod.GET)
      public String message()
      {
          return "hello From BridgeLabz";
+     }
+     //UC2
+     @RequestMapping (value="/queryparam",method = RequestMethod.GET)
+     public String message1(@RequestParam(value = "name", defaultValue = "BridgeLabz") String name)
+     {
+
+         return "Hello "+name;
      }
 }
